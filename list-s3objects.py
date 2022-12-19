450edc7c06cf4504866d006015da5b4c4a6dccb4
ilhamri <115104463+ilhamri@users.noreply.github.com>
2022-12-19 12:04:27 -0500

    Create list-s3objects.py

---
+++ list-s3objects.py
@@ -0,0 +1,10 @@
+import boto3
+s3_resource=boto3.client("s3")
+objects=s3_resource.list_objects(Bucket="faali")["Contents"] #enter your bucket name
+len(objects)
+
+if len(objects)>0:
+    print("objects exits")
+    
+for object in objects:
+    print(object["Key"])
