063cb9acc2a34fdfa62d3d266babfc6fd8ecdfa0
EC2 Default User <ec2-user@ip-172-31-38-70.us-east-2.compute.internal>
2022-12-19 20:28:22 +0000

    create a EBS volume

---
+++ EBS.py
@@ -0,0 +1,11 @@
+
+
+# create AWS EBS volume from Snapshot using boto3 python
+
+import boto3
+ec2_client=boto3.client("ec2")
+ec2_client.create_volume(AvailabilityZone='us-east-1c',
+    Encrypted=True,
+    Size=12,
+    SnapshotId='snap-036da9245a2ab2516',
+    VolumeType='gp2')
