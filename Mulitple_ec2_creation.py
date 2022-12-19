7f46bc1c04390897a6e3d988b19516f8daf37753
ilhamri <115104463+ilhamri@users.noreply.github.com>
2022-12-19 15:07:59 -0500

    Delete Mulitple_ec2_creation.py

--- Mulitple_ec2_creation.py
+++
@@ -1,8 +0,0 @@
-# create multiple EC2 instances boto3
-
-import boto3
-ec2_resource=boto3.resource("ec2")
-ec2_resource.create_instances(ImageId='ami-0bfdcb5067f052a63',
-        InstanceType='t2.micro',
-        MaxCount=3,
-        MinCount=3)
